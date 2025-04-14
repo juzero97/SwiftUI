e1f4fedff76c97691ea011321031215c3f724d07
juzero97 <juzeroo97@gmail.com>
2025-04-14 16:13:51 +0900

    section2 first commit

---
+++ Section2/Section2/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Section2
+//
+//  Created by 송주영 on 4/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+        
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Section2/Section2/Section2App.swift
@@ -0,0 +1,17 @@
+//
+//  Section2App.swift
+//  Section2
+//
+//  Created by 송주영 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Section2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
