ea87b96b0b13516bc17bcb8a4d4b7628aeaccdee
juzero97 <juzeroo97@gmail.com>
2025-04-16 16:22:36 +0900

    section3 project setting

---
+++ Section3/Section3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Section3
+//
+//  Created by 송주영 on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Section3/Section3/Section3App.swift
@@ -0,0 +1,17 @@
+//
+//  Section3App.swift
+//  Section3
+//
+//  Created by 송주영 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Section3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
